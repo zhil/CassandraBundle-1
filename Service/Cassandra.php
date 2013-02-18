922435d5f7cd444f3d2a2ede191957e2ef953283
moldcraft <minecraftmoldova@gmail.com>
2013-02-18 23:01:46 +0200

    simplified embeded structure

--- Service/Cassandra.php
+++ Service/Cassandra.php
@@ -17,28 +17,52 @@ class Cassandra
         $this->exec('USE '. $this->keyspace .';');
     }
 
-    public function exec($cql)
+    public function __call($method, $args)
     {
-        $result = $this->connection->exec($cql);
+        $result = call_user_func_array(array($this->connection, $method), $args);
+
+        if ($method === 'prepare') {
+            $result = new PDOStatement($result);
+        }
 
         $this->check_errors();
 
         return $result;
     }
 
-    public function prepare($cql)
+    protected function check_errors()
     {
-        $result = $this->connection->prepare($cql);
-
-        $this->check_errors();
-
-        return $result;
-    }
-
-    public function check_errors() {
         $error = $this->connection->errorInfo();
 
-        if ($error[1])
+        if ($error[1] !== null)
+            throw new \Exception('Cassandra ['. $error[0] .':'. $error[1] .'] '. $error[2]);
+    }
+}
+
+class PDOStatement
+{
+    protected $stmt = null;
+
+    public function __construct(\PDOStatement $stmt)
+    {
+        $this->stmt         = $stmt;
+        $this->queryString  = $stmt->queryString;
+    }
+
+    public function __call($method, $args)
+    {
+        $result = call_user_func_array(array($this->stmt, $method), $args);
+
+        $this->check_errors();
+
+        return $result;
+    }
+
+    protected function check_errors()
+    {
+        $error = $this->stmt->errorInfo();
+
+        if ($error[1] !== null)
             throw new \Exception('Cassandra ['. $error[0] .':'. $error[1] .'] '. $error[2]);
     }
 }
